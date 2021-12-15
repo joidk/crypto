c5ac4955ec28314eebdfa8c60ae94954ab1d8eaf
joidk <joidk.projects@gmail.com>
2021-12-15 22:16:37 +0000

    s

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,46 +1,69 @@
-#include <cmath>
-#include <vector>
-#include <iostream>
+#include "Crypto.h"
+
+unsigned int Crypto::RGB::matrix[3][3] = {{0, 2, 1},
+                                          {2, 1, 0},
+                                          {1, 0, 2}};
+
 /*
-void changeBase (int n, int m, int* nmsg) {
+ *  
+ *
+void Crypto::RGB::decToTer(int* decmsg) {
+    int size = sizeof(decmsg) / sizeof(int);
+    int termsg[size][3];
 
-int size = sizeof(nmsg)/sizeof(int);
-int mmsg[size];
-	if(n != 10) {
-		std::vector< std::vector<int> > v_mmsg(size);
-		for(int i = 0; i < size; ++i) {
-			for (int j = 0; j > -1; ++j) {
-				int rest = nmsg[i]%10;
-				nmsg[i] = (int)nmsg[i]/10;
-				mmsg[i] = pow(rest,j);
-				if(nmsg[i]<1) break;
-			}
-		}
-		std::cout << nmsg[0] << std::endl;
-	}
-}
+    for(int j = 0; j < size; ++j) {
+        int i = 2;
+        do {
+            if(decmsg[j] != 0) {
+                termsg[j][i] = decmsg[j] % 3;
+                decmsg[j] = (int)decmsg[j] / 3;
+            } else
+              termsg[j][i] = 0;
+            --i;
+        } while(i > -1);
+    }
 */
-void decToTer(int* decmsg) {
-
-	int size = sizeof(decmsg)/sizeof(int);
-	int termsg[size][3]; 
-
-	for(int j = 0; j < size; ++j) {
-		int i = 2;
-		do{
-			if(decmsg[j] != 0){
-				termsg[j][i] = decmsg[j]%3;
-				decmsg[j] = (int)decmsg[j]/3;
-			}
-			else termsg[j][i] = 0;
-			--i;
-		} while(i > -1);
-	}
-
+/*
+ *  Produces "cross" operation 
+ */
+unsigned int Crypto::RGB::dot(int i, int j) {
+  return RGB::matrix[i][j];
 }
 
-int main() {
-	int i[2] = {2,11};
-	decToTer(i);
-	return 0;
+void Crypto::RGB::showDot() {
+  for(int i = 0; i < 3; i++)
+    for(int j = 0; j < 3; j ++)
+      printf("\t<%d, %d> = %d\n", i, j, Crypto::RGB::matrix[i][j]);
 }
+
+/*
+ *  Show message on screen
+ */
+void Crypto::showMsg() {
+  std::cout << "MESSAGE:\t" << this->msg << std::endl;
+}
+
+
+/*
+ *  Converts char to decimal
+ */
+void Crypto::charToDec() {
+  
+  unsigned int decArr[msg.size()], size = msg.size();
+  for(int i = 0; i < size; i++) {
+    if('A' <= msg[i] && msg[i] <= 'Z')
+      msg[i] = msg[i] - ('A' - 'a');
+
+    decArr[i] = msg[i] - 'a';
+    if(msg[i] < 'a' || msg[i] > 'z')
+      decArr[i] = 26;
+  }
+}
+
+char* Crypto::RGB::getCode() {
+  printf("%d, %d, %d\n", this->ternary[0], this->ternary[1], this->ternary[2]);
+  //printf("[%c, %c, %c]", code[2], code[1], code[0]);
+  return this->code;
+}
+
+
