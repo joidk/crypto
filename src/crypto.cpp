c52bb09de7e47cf14770041f5e88867ad3ef4026
joidk <joidk.projects@gmail.com>
2021-12-19 06:08:37 -0800

    d

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,7 +1,6 @@
 #include "Crypto.h"
 
-<<<<<<< HEAD
-char symbols[3] = { 'R', 'G', 'B' }; 
+char Crypto::RGB::symbols[3] = { 'R', 'G', 'B' }; 
 unsigned int Crypto::RGB::matrix[3][3] = {{0, 2, 1},
                                           {2, 1, 0},
                                           {1, 0, 2}};
@@ -29,17 +28,10 @@ Crypto::RGB::RGB(std::string mes_) {
 
   convert(); 
 }
-=======
-char Crypto::RGB::symbols[3]            = { 'R', 'G', 'B' };
-unsigned int Crypto::RGB::matrix[3][3]  = { {0, 2, 1},
-                                            {2, 1, 0},
-                                            {1, 0, 2} };
->>>>>>> 2612c0e6eec9add63047bc54ea708046d1cf3150
 
 /*
  *  Produces operation 
  */
-<<<<<<< HEAD
 void Crypto::RGB::decToTern() {
     
   for (int i = 0; i < Nchars; ++i) {
@@ -64,16 +56,16 @@ void Crypto::RGB::decToTern() {
     }
 
     ternArray = termsg;
-=======
+  }
+
 unsigned int Crypto::RGB::dot(int i, int j) {
   return RGB::matrix[i][j];
->>>>>>> 2612c0e6eec9add63047bc54ea708046d1cf3150
 }
 
 /*
  *  Checks if a certain number is RGB codable
  */
-<<<<<<< HEAD
+
 void Crypto::RGB::ternToCodedRGB() {
   /*int r0, r1, r2;
   for(int i = 0; i < this->Nchars; i++) {
@@ -82,8 +74,8 @@ void Crypto::RGB::ternToCodedRGB() {
     r2 = RGB::matrix[ r2 ][ ternArray[i][2] ];
 
     printf("(%c, %c, %c)\t", symbols[r0], symbols[r1], symbols[r2]); 
-  }*/
-=======
+  */}
+
 bool isCodable(int num) {
   int csize = sizeof(Crypto::RGB::symbols) / sizeof(*Crypto::RGB::symbols);
   for(int i = 0; i < csize; i++)
@@ -92,34 +84,13 @@ bool isCodable(int num) {
   return false;
 }
 
-void Crypto::RGB::showDot() {
-  for(int i = 0; i < 3; i++)
-    for(int j = 0; j < 3; j ++)
-      printf("\t<%d, %d> = %d\n", i, j, Crypto::RGB::matrix[i][j]);
->>>>>>> 2612c0e6eec9add63047bc54ea708046d1cf3150
-}
-
-/*
- *  Show message on screen
- */
-void Crypto::showMsg() {
-  std::cout << "MESSAGE:\t" << this->msg << std::endl;
-}
-
-
 /*
  *  Converts char to decimal
  */
-<<<<<<< HEAD
 void Crypto::RGB::strToDec() {
-  int decArr[msg.size()];
-  int n = msg.size();
-  for(int i = 0; i < n; i++) {
-=======
-void Crypto::charToDec() {
-  unsigned int decArr[msg.size()], size = msg.size();
+  int decArr[msg.size()], size = msg.size();
   for(int i = 0; i < size; i++) {
->>>>>>> 2612c0e6eec9add63047bc54ea708046d1cf3150
+
     if('A' <= msg[i] && msg[i] <= 'Z')
       msg[i] = msg[i] - ('A' - 'a');
 
@@ -127,18 +98,8 @@ void Crypto::charToDec() {
     if(msg[i] < 'a' || msg[i] > 'z')
       decArr[i] = 26;
   }
-}
-
-<<<<<<< HEAD
   static int* cop = decArr;
   decimalArray = cop;
-
-=======
-char* Crypto::RGB::getCode() {
-  printf("%d, %d, %d\n", this->ternary[0], this->ternary[1], this->ternary[2]);
-  //printf("[%c, %c, %c]", code[2], code[1], code[0]);
-  return this->code;
->>>>>>> 2612c0e6eec9add63047bc54ea708046d1cf3150
 }
 
 
